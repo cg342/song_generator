3ebfc33f3c2bb69f8019f088dffc25b540646c96
krissykrissy <shinesinthesun@yahoo.com>
2018-08-17 17:34:04 -0400

    minor edit

--- song_generator.py
+++ song_generator.py
@@ -11,15 +11,14 @@ import functions as fun
 fast = fun.readSongs("fast.txt")
 slow = fun.readSongs("slow.txt")
 history = fun.readHistory()
-for h in history:
-  print h
+
 
 # get intput from console
 numbers = fun.getInput()
 
 while True:
-  output = fun.getSong(numbers,0,fast, slow, history)
-  output.extend(fun.getSong(numbers,1,fast, slow, history))
+  output = fun.getSong(numbers, 0, fast, slow, history)
+  output.extend(fun.getSong(numbers, 1, fast, slow, history))
   fun.printResult(numbers, output)
   print "Enter 1: write history, 2: repeat 3: exit"
   cont = raw_input()
